3bc6cbced2a23828eb08114d35ea1148a88c11e3
Pablo Tack <tackpablo@gmail.com>
2022-02-17 09:16:10 -0800

    test and logic completed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,45 @@
 const request = require("request");
 
-const breed = process.argv.slice(2);
-const apiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
+const fetchBreedDescription = function (breed, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      callback(`Failed to request details. Details: `, null);
+    }
 
-request(`${apiUrl}${breed}`, (error, response, body) => {
-  // Request failed
-  if (error) {
-    return console.log(`Failed to request details. Details: `, error);
-  }
-  // console.log("error:", error);
-  // console.log("body:", body);
-  // console.log("response: ", response);
+    const data = JSON.parse(body); // parse the JSON into object
 
-  // Breed not found
-  if (error === null) {
-    return console.log(`Requested breed could not be found.`);
-  }
+    if (!data[0]) callback(undefined, undefined);
+    else callback(undefined, data[0].description);
+  });
+};
 
-  // console.log(body); // body is JSON
-  // console.log(typeof body); // body is type string
+module.exports = { fetchBreedDescription };
 
-  const data = JSON.parse(body); // parse the JSON into object
-  // console.log(data); // check the data
-  // console.log(typeof data); // data is type object
+// old code
+// const breed = process.argv.slice(2);
+// const apiUrl = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-  console.log(data[0].description);
-});
+// request(`${apiUrl}${breed}`, (error, response, body) => {
+//   // Request failed
+//   if (error) {
+//     return console.log(`Failed to request details. Details: `, error);
+//   }
+//   // // console.log("error:", error);
+//   // // console.log("body:", body);
+//   // // console.log("response: ", response);
+
+//   // Breed not found
+//   if (error === null) {
+//     return console.log(`Requested ${breed} could not be found.`);
+//   }
+
+//   // console.log(body); // body is JSON
+//   // console.log(typeof body); // body is type string
+
+//   const data = JSON.parse(body); // parse the JSON into object
+//   // console.log(data); // check the data
+//   // console.log(typeof data); // data is type object
+
+//   console.log(data[0].description);
+// });
---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const { fetchBreedDescription } = require("./breedFetcher");
+
+const breed = process.argv[2];
+
+fetchBreedDescription(breed, (error, desc) => {
+  if (error) {
+    console.log("Error fetch details:", error);
+  } else {
+    console.log(desc);
+  }
+});
